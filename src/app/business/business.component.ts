8bd6b8153ed7a4ad69f61dfedd4ad7a25d25858e
motasem salem <mmsalem17@cit.just.edu.jo>
2020-12-31 18:08:22 +0200

    init

--- src/app/business/business.component.ts
+++ src/app/business/business.component.ts
@@ -1,17 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
-import { BusinessRoutingConstants } from '@app/core/constants/routes';
-import { SharedConstants } from '@app/core/constants/constants';
-import { AuthService } from '@app/core/services/auth/auth.service';
+import { Component, OnInit } from "@angular/core";
+import { TranslateService } from "@ngx-translate/core";
+import { BusinessRoutingConstants } from "@app/core/constants/routes";
+import { SharedConstants } from "@app/core/constants/constants";
+import { AuthService } from "@app/core/services/auth/auth.service";
 
-import { ActivatedRoute } from '@angular/router';
-import { MenuController } from '@ionic/angular';
-
+import { ActivatedRoute } from "@angular/router";
+import { MenuController } from "@ionic/angular";
 
 @Component({
-  selector: 'app-component',
-  templateUrl: './business.component.html',
-  styleUrls: ['./business.component.css']
+  selector: "app-component",
+  templateUrl: "./business.component.html",
+  styleUrls: ["./business.component.css"],
 })
 export class BusinessComponent implements OnInit {
   public selectedIndex = 0;
@@ -21,71 +20,74 @@ export class BusinessComponent implements OnInit {
 
   garageOwnerPages = [
     {
-      title: 'My Store',
+      title: "My Store",
       url: `/${BusinessRoutingConstants.BUSINESS}/${BusinessRoutingConstants.MY_STORES}`,
-      icon: 'accessibility'
+      icon: "accessibility",
     },
   ];
 
   CarOwnerPages = [
-
     {
-      title: 'My Cars',
-      url: '/business/my-cars',
-      icon: 'car-sport'
+      title: "My Cars",
+      url: "/business/my-cars",
+      icon: "car-sport",
     },
   ];
   guestPages = [
     {
-      title: 'STORES',
-      url: '/business/store',
-      icon: 'storefront'
+      title: "STORES",
+      url: "/business/store",
+      icon: "storefront",
     },
     {
-      title: 'PRODUCTS',
-      url: '/business/products',
-      icon: 'cube'
+      title: "PRODUCTS",
+      url: "/business/products",
+      icon: "cube",
     },
     {
-      title: 'OFFERS',
-      url: '/business/offers',
-      icon: 'gift'
+      title: "OFFERS",
+      url: "/business/offers",
+      icon: "gift",
     },
     {
-      title: 'SHOPPING_CART',
-      url: '/business/shopping-card',
-      icon: 'bag-handle'
+      title: "SHOPPING_CART",
+      url: "/business/shopping-card",
+      icon: "bag-handle",
     },
-  ]
+  ];
 
   public appPages = [
     {
-      title: 'Search By Image',
-      url: '/business/search-by-image',
-      icon: 'search',
+      title: "Search By Image",
+      url: "/business/search-by-image",
+      icon: "search",
     },
     {
-      title: 'CHAT',
-      url: '/business/chat',
-      icon: 'mail'
+      title: "CHAT",
+      url: "/business/chat",
+      icon: "mail",
     },
     {
-      title: 'SETTING',
-      url: '/business/settings',
-      icon: 'settings'
-    },
-  ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
-  role: string;
+      title: "SETTING",
+      url: "/business/settings",
+      icon: "settings",
 
+    },
+
+  ];
+  public labels = ["Family", "Friends", "Notes", "Work", "Travel", "Reminders"];
+  role: string;
+  isLoggedIn: boolean = false;
   constructor(
     private activatedRoute: ActivatedRoute,
     private translate: TranslateService,
-    private authService: AuthService,
-    // private menu: MenuController
+    private authService: AuthService // private menu: MenuController
   ) {
     this.role = this.authService.getRole();
-    if (this.role === SharedConstants.GUEST || this.role === SharedConstants.CAR_OWNER) {
+    if (
+      this.role === SharedConstants.GUEST ||
+      this.role === SharedConstants.CAR_OWNER
+    ) {
       this.appPages.unshift(...this.guestPages);
       if (this.role === SharedConstants.CAR_OWNER) {
         this.appPages.unshift(...this.CarOwnerPages);
@@ -93,7 +95,9 @@ export class BusinessComponent implements OnInit {
     } else if (this.role === SharedConstants.GARAGE_OWNER) {
       this.appPages.unshift(...this.garageOwnerPages);
     } else {
-
+    }
+    if (this.authService.loggedIn) {
+     this.isLoggedIn = true;
     }
     // this.menu.enable(true, 'mainContent')
     this.username = this.authService.getUsername();
@@ -111,12 +115,17 @@ export class BusinessComponent implements OnInit {
   //   //   }
   //   // })
   // }
-  ngOnInit() {
-    const path = window.location.pathname.split('folder/')[1];
-    this.folder = this.activatedRoute.snapshot.paramMap.get('id');
-    if (path !== undefined) {
-      this.selectedIndex = this.appPages.findIndex(page => page.title.toLowerCase() === path.toLowerCase());
-    }
+  logout() {
+    this.authService.logout();
   }
 
+  ngOnInit() {
+    const path = window.location.pathname.split("folder/")[1];
+    this.folder = this.activatedRoute.snapshot.paramMap.get("id");
+    if (path !== undefined) {
+      this.selectedIndex = this.appPages.findIndex(
+        (page) => page.title.toLowerCase() === path.toLowerCase()
+      );
+    }
+  }
 }
--- src/app/core/services/auth/auth.service.ts
+++ src/app/core/services/auth/auth.service.ts
@@ -1,20 +1,26 @@
-import { Injectable, Pipe } from '@angular/core';
-import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
-import { Observable, Subject, throwError } from 'rxjs';
-import { BasedUrlsConstants } from '@app/core/constants/routes';
-import { Router } from '@angular/router';
-import { AppRoutingConstants, BusinessRoutingConstants } from '@app/core/constants/routes';
-import { TokenHandlerService } from '@app/core/services/token-handler/token-handler.service';
-import { catchError } from 'rxjs/operators';
+import { Injectable, Pipe } from "@angular/core";
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpHeaders,
+} from "@angular/common/http";
+import { Observable, Subject, throwError } from "rxjs";
+import { BasedUrlsConstants } from "@app/core/constants/routes";
+import { Router } from "@angular/router";
+import {
+  AppRoutingConstants,
+  BusinessRoutingConstants,
+} from "@app/core/constants/routes";
+import { TokenHandlerService } from "@app/core/services/token-handler/token-handler.service";
+import { catchError } from "rxjs/operators";
 // import jwt from 'jsonwebtoken';
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class AuthService {
-
   subject: Subject<{}>;
   basedUrl = BasedUrlsConstants.BASED_URL_LOCALHOST;
-  private headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+  private headers = new HttpHeaders({ "Content-Type": "application/json" });
   token: string;
   user: any;
 
@@ -28,8 +34,8 @@ export class AuthService {
   }
 
   getData() {
-    if (localStorage.getItem('access_token')) {
-      this.token = localStorage.getItem('access_token');
+    if (localStorage.getItem("access_token")) {
+      this.token = localStorage.getItem("access_token");
       this.user = this.tokenHandlerService.getPayload(this.token);
     }
   }
@@ -43,35 +49,45 @@ export class AuthService {
   }
 
   getRole() {
-    return (this.user && this.user.role) || 'guest';
+    return (this.user && this.user.role) || "guest";
   }
   getUsername() {
-    return (this.user && this.user.username) || 'guest';
+    return (this.user && this.user.username) || "guest";
   }
   signUpForGarageOwner(data): Observable<any> {
-    return this.http.post(this.basedUrl + 'user/garage-owner/create', data);
+    return this.http.post(this.basedUrl + "user/garage-owner/create", data);
   }
   signUpForCarOwner(data): Observable<any> {
-    return this.http.post(this.basedUrl + 'user/car-owner/create', data);
+    return this.http.post(this.basedUrl + "user/car-owner/create", data);
+  }
+
+  logout() {
+    this.http.delete(this.basedUrl + "user/logout").subscribe(() => {
+      localStorage.removeItem("access_token");
+      this.user = null;
+      this.token = null;
+      this.router.navigateByUrl(`/${AppRoutingConstants.AUTH}`);
+    });
   }
 
   clearDate() {
-    localStorage.removeItem('access_token');
+    localStorage.removeItem("access_token");
     this.user = null;
     this.token = null;
   }
   login(data) {
-    this.http.post(this.basedUrl + 'user/login', data, { headers: this.headers }).pipe(catchError(this.error)).subscribe(async (res: any) => {
-      if (res.auth) {
-        localStorage.setItem('access_token', res.token);
-        this.token = res.token;
-        this.user = this.tokenHandlerService.getPayload(this.token);
-        this.router.navigateByUrl(`/${AppRoutingConstants.BUSINESS}`);
-      } else {
-
-      }
-    });
-
+    this.http
+      .post(this.basedUrl + "user/login", data, { headers: this.headers })
+      .pipe(catchError(this.error))
+      .subscribe(async (res: any) => {
+        if (res.auth) {
+          localStorage.setItem("access_token", res.token);
+          this.token = res.token;
+          this.user = this.tokenHandlerService.getPayload(this.token);
+          this.router.navigateByUrl(`/${AppRoutingConstants.BUSINESS}`);
+        } else {
+        }
+      });
   }
 
   public get loggedIn(): boolean {
@@ -80,16 +96,15 @@ export class AuthService {
   private error(error: HttpErrorResponse) {
     if (error.error instanceof ErrorEvent) {
       // A client-side or network error occurred. Handle it accordingly.
-      console.error('An error occurred:', error.error.message);
+      console.error("An error occurred:", error.error.message);
     } else {
       // The backend returned an unsuccessful response code.
       // The response body may contain clues as to what went wrong,
       console.error(
-        `Backend returned code ${error.status}, ` +
-        `body was: ${error.error}`);
+        `Backend returned code ${error.status}, ` + `body was: ${error.error}`
+      );
     }
     // return an observable with a user-facing error message
-    return throwError(
-      'Something bad happened; please try aga<  in later.');
+    return throwError("Something bad happened; please try aga<  in later.");
   }
 }
